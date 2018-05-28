3fb4726c32e1300c88e78dc3a17a2bdf1b15f48c
Albin Engman <albin.a.engman@gmail.com>
2018-05-28 10:51:51 +0200

    Small fixed

--- src/components/Products/productList.js
+++ src/components/Products/productList.js
@@ -14,16 +14,22 @@ class ProductList extends Component {
           <div className="card">
             <div className="card-header">{product.name}</div>
                 <div className="card-main">
-                <img src={product.picture} alt="Avatar"/>
-                <div className="main-description">{product.description}</div>
-                <div>{product.price} &#36;</div>
-                {product.amount === 0 || product.amount === '0' ? <div><br /><div>Out of Stock</div></div> : <div><button data-name={product.name} data-description={product.description} data-picture={product.picture} data-amount={product.amount} data-price={product.price} onClick={event => {
-                      {this.handleAddClick(event)}
-                    }}>Add To Cart</button>
-                <div>In Stock: {product.amount}</div></div> }
-                    
+                  <img src={product.picture} alt="Avatar"/>
+                  <div className="main-description">{product.description}</div>
+                  <div>{product.price} &#36;</div>
+                  {product.amount < 1 || product.amount === '0' ?
+                      <div><br />
+                        <div>Out of Stock</div>
+                      </div>
+                    :
+                    <div>
+                      <button data-name={product.name} data-description={product.description} data-picture={product.picture} data-amount={product.amount} data-price={product.price} onClick={event => {
+                      {this.handleAddClick(event)}}}>Add To Cart</button>
+                      <div>In Stock: {product.amount}</div>
+                    </div>
+                  }
             </div>
-            </div>
+          </div>
         </div>
       )) // Dataset contains all element unformation required in the button. (Figure out a diffrenet solution?)
 
